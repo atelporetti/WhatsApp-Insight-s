6e550597f32998636dd84315edb7bd94733e7f63
atelito <aaxelporetti@gmail.com>
2021-02-23 19:11:10 -0300

    regex mejorada

--- 1.py
+++ 1.py
@@ -1,2 +1,35 @@
-a = 'akaknaka\n'
-print(a[:-1])
+import os, re
+direccion = os.path.join('Assets', 'chat.txt')
+
+def es_comienzo_linea(linea):
+        #El formato siempre es: <datetime><separator><contact/phone number>
+        #(?P<datetime>(\[?)(((\d{1,2})(/|-)(\d{1,2})(/|-)(\d{2,4}))(,?\s)((\d{1,2})(:|\.)(\d{2})(\.|:)?(\d{2})?(\s?[apAP]\.?[mM]\.?)?))(\]?\s-?\s?))(?P<sender>(.*?))(:+\s?)(?P<message>(.+))
+        patron = r"""
+            (\[?)#Cero o Un corchete '['
+            (?P<datetime>#Agrupo caracteres con el nombre 'datetime'
+            (((\d{1,2})#1 a 2 digitos de dia
+            (/|-)#'/' o '-' separador de dia 
+            (\d{1,2})#1 a 2 digitos de mes
+            (/|-)#'/' o '-' separador de mes
+            (\d{2,4}))#2 a 4 digitos de año
+            (,?\s)#Cero o Una coma ',' y un espacio en blanco
+            ((\d{1,2})#Exactamente 1 a 2 digitos de hora
+            (:|\.)#Separadores de Dos puntos ':' o punto '.'
+            (\d{2})#2 digitos exactos para minutos
+            (\.|:)?#Cero o Un punto '.' o dos puntos ':'
+            (\d{2})?#Cero o Exactamente 2 digitos para segundos
+            (\s?[apAP]\.?[mM]\.?)?)))#Cero o un caracter de ('espacio', 'A' or 'P', and 'M') para formato de AM/PM
+            (\]?\s-?\s?)#Cero o Un corchete ']', Cero o Un (espacio y '-'), cero o Un espacio en blanco
+            (?P<sender>#Agrupo caracteres con el nombre 'sender'
+            (.*?))#Cero o mas caracteres (excepto \n) en forma lazy
+            (:+\s?)#Uno o mas caracteres de dos puntos ':', cero o Un espacio en blacno
+            (?P<message>#Agrupo caracteres con el nombre 'message'
+            (.+))#Uno o mas caracteres (excepto \n) de mensajes
+        """
+        match = re.match(re.compile(patron, re.VERBOSE), linea)
+        if match:
+            return match
+        return None
+
+resultado = es_comienzo_linea('‎[14/6/17 19:30:23] AP : ‎audio omitido')
+print(resultado)
--- etl.py
+++ etl.py
@@ -4,17 +4,46 @@ import pandas as pd
 
 
 direccion = os.path.join('Assets', 'chat.txt')
-patron_1 = '(?P<datetime>\d+/\d+/\d+ \d{2}:\d{2}) - (?P<sender>(.*?)): (?P<message>((.+\s+)?))'
-patron_2 = '(?P<datetime>\[\d+\/\d+\/\d+ \d{2}:\d{2}:\d{2}\]) (?P<sender>(.*?)): (?P<message>((.+\s+)?))'
+
+def es_comienzo_linea(linea):
+        #El formato siempre es: <datetime><separator><contact/phone number>
+        # (?P<datetime>(\[?)(((\d{1,2})(/|-)(\d{1,2})(/|-)(\d{2,4}))(,?\s)((\d{1,2})(:|\.)(\d{2})(\.|:)?(\d{2})?(\s?[apAP]\.?[mM]\.?)?))(\]?\s-?\s?))(?P<sender>(.*?))(:+\s?)(?P<message>(.+))
+        patron = r"""
+            (?P<datetime>#Agrupo caracteres con el nombre 'datetime'
+            (\[?)       #Cero o Un corchete '['
+            (((\d{1,2}) #1 a 2 digitos de dia
+            (/|-)       #'/' o '-' separador de dia 
+            (\d{1,2})   #1 a 2 digitos de mes
+            (/|-)       #'/' o '-' separador de mes
+            (\d{2,4}))  #2 a 4 digitos de año
+            (,?\s)      #Cero o Una coma ',' y un espacio en blanco
+            ((\d{1,2})  #Exactamente 1 a 2 digitos de hora
+            (:|\.)      #Separadores de Dos puntos ':' o punto '.'
+            (\d{2})     #2 digitos exactos para minutos
+            (\.|:)?     #Cero o Un punto '.' o dos puntos ':'
+            (\d{2})?    #Cero o Exactamente 2 digitos para segundos
+            (\s?[apAP]\.?[mM]\.?)?))  #Cero o un caracter de ('espacio', 'A' or 'P', and 'M') para formato de AM/PM
+            (\]?\s-?\s?))#Cero o Un corchete ']', Cero o Un (espacio y '-'), cero o Un espacio en blanco
+            (?P<sender> #Agrupo caracteres con el nombre 'sender'
+            (.*?))      #Cero o mas caracteres (excepto \n) en forma lazy
+            (:+\s?)      #Uno o mas caracteres de dos puntos ':', cero o Un espacio en blacno
+            (?P<message>#Agrupo caracteres con el nombre 'message'
+            (.+))       #Uno o mas caracteres (excepto \n) de mensajes
+        """
+        match = re.match(re.compile(patron, re.VERBOSE), linea)
+        if match:
+            return match
+        return None
 
 
-def comprueba_patron(cadena):
+
+""" def comprueba_patron(cadena):
     if re.match(patron_1, cadena):
         return patron_1
     elif re.match(patron_2, cadena):
         return patron_2,
     else:
-        return 
+        return patron_vacio
 
 
 def empieza_con_fecha(cadena):
@@ -23,27 +52,28 @@ def empieza_con_fecha(cadena):
     elif re.match(patron_2, cadena):
         return re.match(patron_2, cadena)
     else:
-        return False
+        return re.match(patron_vacio, cadena) """
 
 
 def lee_chat(dir):
     lineas = []
-    with open(dir, encoding="utf8") as archivo:
-        for linea in archivo:
-            #linea = linea.strip()
-            if empieza_con_fecha(linea):
-                resultado = re.findall(patron_1, linea[:-1])
+    try:
+        with open(dir, encoding="utf8") as archivo:
+            for linea in archivo:
+                #linea.replace('[', '').replace(']', '')
+                resultado = es_comienzo_linea(linea)
                 for columna in resultado:
                     linea = [columna[0].strip('[').strip(']'), columna[1], columna[3]]
                     lineas.append(linea)
-            #else:
-                #pass
-                #resultado = linea.strip()
-                # for columna in resultado:
-                # print(resultado)
-            # print(lineas[0])
-    return lineas
-
+                #else:
+                    #pass
+                    #resultado = linea.strip()
+                    # for columna in resultado:
+                    # print(resultado)
+                # print(lineas[0])
+        return lineas
+    except IOError as e:
+        print(f"Archivo {direccion} no encontrado. Please recheck your file location")
 
 # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DatetimeIndex.html
 df = pd.DataFrame(lee_chat(direccion), columns=['datetime', 'sender', 'message'])

877cf60451786481ee450aa969f06094229fa046
atelito <aaxelporetti@gmail.com>
2021-02-23 16:01:04 -0300

    segundo patron

--- etl.py
+++ etl.py
@@ -3,41 +3,50 @@ import re
 import pandas as pd
 
 
-direccion = os.path.join('Assets', '_chat.txt')
+direccion = os.path.join('Assets', 'chat.txt')
 patron_1 = '(?P<datetime>\d+/\d+/\d+ \d{2}:\d{2}) - (?P<sender>(.*?)): (?P<message>((.+\s+)?))'
 patron_2 = '(?P<datetime>\[\d+\/\d+\/\d+ \d{2}:\d{2}:\d{2}\]) (?P<sender>(.*?)): (?P<message>((.+\s+)?))'
+
 
 def comprueba_patron(cadena):
     if re.match(patron_1, cadena):
         return patron_1
     elif re.match(patron_2, cadena):
         return patron_2,
+    else:
+        return 
+
 
 def empieza_con_fecha(cadena):
-    resultado = re.match(comprueba_patron(cadena), cadena)
-    if resultado:
-        return True
-    return False
+    if re.match(patron_1, cadena):
+        return re.match(patron_1, cadena)
+    elif re.match(patron_2, cadena):
+        return re.match(patron_2, cadena)
+    else:
+        return False
 
-lineas = []
-with open(direccion, encoding="utf8") as archivo:
-    for linea in archivo:
-        #linea = linea.strip()
-        if empieza_con_fecha(linea):
-            resultado = re.findall(comprueba_patron(linea), linea[:-1])
-            for columna in resultado:
-                linea = [columna[0], columna[1], columna[3]]
-                lineas.append(linea)
-        else:
-            resultado = linea.strip()
-            #for columna in resultado:
-            print(resultado)
-            #print(lineas[0])
 
+def lee_chat(dir):
+    lineas = []
+    with open(dir, encoding="utf8") as archivo:
+        for linea in archivo:
+            #linea = linea.strip()
+            if empieza_con_fecha(linea):
+                resultado = re.findall(patron_1, linea[:-1])
+                for columna in resultado:
+                    linea = [columna[0].strip('[').strip(']'), columna[1], columna[3]]
+                    lineas.append(linea)
+            #else:
+                #pass
+                #resultado = linea.strip()
+                # for columna in resultado:
+                # print(resultado)
+            # print(lineas[0])
+    return lineas
 
 
 # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DatetimeIndex.html
-df = pd.DataFrame(lineas, columns=['datetime', 'sender', 'message'])
+df = pd.DataFrame(lee_chat(direccion), columns=['datetime', 'sender', 'message'])
 df['datetime'] = pd.to_datetime(df['datetime'])
 df['date'] = df['datetime'].dt.date
 df['time'] = df['datetime'].dt.time
